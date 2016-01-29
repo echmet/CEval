968c3820b27e8d9d6b8f80942049e8f00c8c121d
Michal Malý <malymi@natur.cuni.cz>
2016-01-29 21:29:13 +0100

    Version bump

--- src/globals.cpp
+++ src/globals.cpp
@@ -4,8 +4,8 @@ const QString Globals::ECHMET_WEB_LINK = "http://echmet.natur.cuni.cz";
 const QString Globals::ORG_NAME = "ECHMET";
 const QString Globals::SOFTWARE_NAME = "CEval";
 const int Globals::VERSION_MAJ = 0;
-const int Globals::VERSION_MIN = 2;
-const QString Globals::VERSION_REV = "rc10";
+const int Globals::VERSION_MIN = 3;
+const QString Globals::VERSION_REV = "rc1";
 
 const QVector<Globals::DeveloperID> Globals::DEVELOPERS = { Globals::DeveloperID("Pavel Dubsk\u00FD", "pavel.dubsky@natur.cuni.cz"),
                                                             Globals::DeveloperID("Magda \u00D6rd\u00F6gov\u00E1", "magda.ordogova@natur.cuni.cz"),
