d1236ea75d604ba38037e79218ced404a3c3b155
Michal Malý <malymi@natur.cuni.cz>
2016-05-17 17:19:50 +0200

    Move non-template functions out of the header file

--- src/evalserializable.cpp
+++ src/evalserializable.cpp
@@ -1,3 +1,237 @@
 #include "evalserializable.h"
 
 const std::string EvalSerializable::HEADER_TAG = "CUTEEVAL_1.0_DATAFILE";
+
+EvalSerializable::DataManipulator::DataManipulator(const EvalSerializable& Subject, ByteStream *Stream) :
+  m_Subject(Subject),
+  m_Stream(Stream),
+  m_Ptr(nullptr),
+  m_End(nullptr),
+  m_Direction(EVS_OUT)
+{
+}
+EvalSerializable::DataManipulator::DataManipulator(const EvalSerializable& Subject, const char **Ptr, const char * const End) :
+  m_Subject(Subject),
+  m_Stream(nullptr),
+  m_Ptr(Ptr),
+  m_End(End),
+  m_Direction(EVS_IN)
+{
+}
+
+EvalSerializable::EvalSerializable() :
+  m_LastInternalError(EvalSerializable::SUCCESS)
+{
+}
+
+EvalSerializable::~EvalSerializable()
+{
+}
+
+EvalSerializable::RetCode EvalSerializable::CheckHeader(const char*& Ptr, const char * const End) const
+{
+  std::string Header;
+  std::string TN;
+
+  ReadData(Header, Ptr, End);
+  if (Header.compare(HEADER_TAG) != 0)
+    return ERR_UNKWFILE;
+
+  ReadData(TN, Ptr, End);
+  if (TN.compare(TypeName()) != 0)
+    return ERR_BADTYPE;
+
+  return SUCCESS;
+}
+
+EvalSerializable::RetCode EvalSerializable::DataManipulator::Header()
+{
+  switch (m_Direction) {
+   case EVS_IN:
+     return m_Subject.CheckHeader(*m_Ptr, m_End);
+   case EVS_OUT:
+     return m_Subject.WriteHeader(*m_Stream);
+   default:
+     return ERR_RUNTIME;
+  }
+}
+
+EvalSerializable::RetCode EvalSerializable::Deserialize(const ByteStream& Stream)
+{
+  const char *Ptr = &Stream[0];
+
+  return Deserialize(&Ptr, Ptr + Stream.size());
+}
+
+EvalSerializable::RetCode EvalSerializable::Deserialize(const char **Ptr, const char * const End)
+{
+  DataManipulator DM(*this, Ptr, End);
+  return Operate(DM);
+}
+
+EvalSerializable::RetCode EvalSerializable::Serialize(ByteStream& Stream)
+{
+  DataManipulator DM(*this, &Stream);
+  return Operate(DM);
+}
+
+
+EvalSerializable::RetCode EvalSerializable::ReadFromFile(const char *FileName, EvalSerializable& Object)
+{
+  FILE *Fh;
+  size_t FSize;
+  ByteStream Stream;
+  RetCode Ret;
+
+  Fh = fopen(FileName, "rb");
+  if (Fh == nullptr)
+    return ERR_CANTOPEN;
+
+  fseek(Fh, 0, SEEK_END);
+  FSize = ftell(Fh);
+  fseek(Fh, 0, SEEK_SET);
+
+  Stream.resize(FSize);
+  if (fread(&Stream[0], FSize, 1, Fh) != 1)
+    return ERR_CANTREAD;
+
+  fclose(Fh);
+
+  Ret = Object.Deserialize(Stream);
+  if (Ret != SUCCESS) {
+    Object.m_LastInternalError = Ret;
+    return ERR_CANTDESERIALIZE;
+  }
+
+  return SUCCESS;
+}
+
+std::string EvalSerializable::RetCodeToString(RetCode ret) {
+switch (ret) {
+  case SUCCESS:
+    return "Success";
+  case ERR_BADSIZE:
+    return "Size of the next data block differs from the size of the type";
+  case ERR_BADTYPE:
+    return "Object types do not match";
+  case ERR_CANTDESERIALIZE:
+    return "Object could not have been deserialized";
+  case ERR_CANTOPEN:
+    return "Cannot open file";
+  case ERR_CANTREAD:
+    return "Cannot read from file";
+  case ERR_CANTSERIALIZE:
+    return "Object could not have been serialized";
+  case ERR_CANTWRITE:
+    return "Cannt write to file";
+  case ERR_NOMEM:
+    return "Out of memory";
+  case ERR_OUTOFRANGE:
+    return "Attempted to read past the bytestream size";
+  case ERR_RUNTIME:
+    return "Runtime error";
+  case ERR_UNKWFILE:
+    return "Unknown type of file";
+  default:
+    return "Unknown error";
+  }
+}
+
+EvalSerializable::RetCode EvalSerializable::WriteToFile(const char *FileName, EvalSerializable& Object)
+{
+  FILE *Fh;
+  RetCode Ret;
+  ByteStream Stream;
+
+  Ret = Object.Serialize(Stream);
+
+  if (Ret != SUCCESS) {
+    Object.m_LastInternalError = Ret;
+    return ERR_CANTSERIALIZE;
+  }
+
+  Fh = fopen(FileName, "wb");
+  if (Fh == nullptr)
+    return ERR_CANTOPEN;
+
+  if (fwrite(&Stream[0], Stream.size(), 1, Fh) != 1)
+     return ERR_CANTWRITE;
+
+  fflush(Fh);
+  fclose(Fh);
+
+  return SUCCESS;
+}
+
+EvalSerializable::RetCode EvalSerializable::WriteHeader(ByteStream& Stream) const
+{
+  RetCode Ret;
+
+  Ret = AddData(HEADER_TAG, Stream);
+  if (Ret != SUCCESS)
+    return Ret;
+
+  std::string TN = TypeName();
+  return AddData(TN, Stream);
+}
+
+EvalSerializable::RetCode EvalSerializable::_ReadData(std::string& Data, const char*& Ptr, const char * const End, Identity<std::string>)
+{
+  uint32_t Length;
+  char *Ch;
+
+  Length = *reinterpret_cast<const uint32_t*>(Ptr);
+  Ptr += sizeof(Length);
+
+  if (Ptr + Length > End)
+    return ERR_OUTOFRANGE;
+
+  if (Length == 0) {
+    Data = std::string();
+    return SUCCESS;
+  }
+
+  Ch = new char[Length+1];
+  if (Ch == nullptr)
+    return ERR_NOMEM;
+
+  memcpy(Ch, Ptr, Length);
+  Ch[Length] = '\0';
+  Data = std::string(Ch);
+  Ptr += Length;
+  delete[] Ch;
+
+  return SUCCESS;
+}
+
+EvalSerializable::RetCode EvalSerializable::_ReadData(std::wstring& Data, const char*& Ptr, const char * const End, Identity<std::wstring>)
+{
+  uint32_t Length;
+  uint32_t WLength;
+  wchar_t *Ch;
+
+  Length = *reinterpret_cast<const uint32_t*>(Ptr);
+  Ptr += sizeof(Length);
+
+  if (Ptr + Length > End)
+    return ERR_OUTOFRANGE;
+
+  if (Length == 0) {
+    Data = std::wstring();
+    return SUCCESS;
+  }
+  WLength = Length / sizeof(wchar_t);
+
+
+  Ch = new wchar_t[WLength + 1];
+  if (Ch == nullptr)
+    return ERR_NOMEM;
+
+  memcpy(Ch, Ptr, Length);
+  Ch[WLength] = '\0';
+  Data = std::wstring(Ch);
+  Ptr += Length;
+  delete[] Ch;
+
+  return SUCCESS;
+}
--- src/evalserializable.h
+++ src/evalserializable.h
@@ -44,24 +44,10 @@ protected:
 
   class DataManipulator {
   public:
-    DataManipulator(const EvalSerializable& Subject, ByteStream *Stream) :
-                    m_Subject(Subject), m_Stream(Stream), m_Ptr(nullptr), m_End(nullptr), m_Direction(EVS_OUT)
-                    {}
-    DataManipulator(const EvalSerializable& Subject, const char **Ptr, const char * const End) :
-                    m_Subject(Subject), m_Stream(nullptr), m_Ptr(Ptr), m_End(End), m_Direction(EVS_IN)
-                    {}
+    DataManipulator(const EvalSerializable& Subject, ByteStream *Stream);
+    DataManipulator(const EvalSerializable& Subject, const char **Ptr, const char * const End);
 
-    RetCode Header(void)
-    {
-      switch (m_Direction) {
-      case EVS_IN:
-        return m_Subject.CheckHeader(*m_Ptr, m_End);
-      case EVS_OUT:
-        return m_Subject.WriteHeader(*m_Stream);
-      default:
-        return ERR_RUNTIME;
-      }
-    }
+    RetCode Header();
 
     template<typename T>
     RetCode Operate(T& Data)
@@ -106,147 +92,25 @@ protected:
 public:
   inline RetCode LastError() { return m_LastInternalError; }
 
-  RetCode Deserialize(const ByteStream& Stream)
-  {
-    const char *Ptr = &Stream[0];
-
-    return Deserialize(&Ptr, Ptr + Stream.size());
-  }
-
-  RetCode Deserialize(const char **Ptr, const char * const End)
-  {
-    DataManipulator DM(*this, Ptr, End);
-    return Operate(DM);
-  }
-
-  RetCode Serialize(ByteStream& Stream)
-  {
-    DataManipulator DM(*this, &Stream);
-    return Operate(DM);
-  }
+  RetCode Deserialize(const ByteStream& Stream);
+  RetCode Deserialize(const char **Ptr, const char * const End);
+  RetCode Serialize(ByteStream& Stream);
 
   virtual RetCode Operate(DataManipulator& Manipulator) = 0;
 
-  static RetCode ReadFromFile(const char *FileName, EvalSerializable& Object)
-  {
-    FILE *Fh;
-    size_t FSize;
-    ByteStream Stream;
-    RetCode Ret;
-
-    Fh = fopen(FileName, "rb");
-    if (Fh == nullptr)
-      return ERR_CANTOPEN;
-
-    fseek(Fh, 0, SEEK_END);
-    FSize = ftell(Fh);
-    fseek(Fh, 0, SEEK_SET);
-
-    Stream.resize(FSize);
-    if (fread(&Stream[0], FSize, 1, Fh) != 1)
-      return ERR_CANTREAD;
-
-    fclose(Fh);
-
-    Ret = Object.Deserialize(Stream);
-    if (Ret != SUCCESS) {
-      Object.m_LastInternalError = Ret;
-      return ERR_CANTDESERIALIZE;
-    }
-
-    return SUCCESS;
-  }
-
-  static std::string RetCodeToString(RetCode ret) {
-  switch (ret) {
-    case SUCCESS:
-      return "Success";
-    case ERR_BADSIZE:
-      return "Size of the next data block differs from the size of the type";
-    case ERR_BADTYPE:
-      return "Object types do not match";
-    case ERR_CANTDESERIALIZE:
-      return "Object could not have been deserialized";
-    case ERR_CANTOPEN:
-      return "Cannot open file";
-    case ERR_CANTREAD:
-      return "Cannot read from file";
-    case ERR_CANTSERIALIZE:
-      return "Object could not have been serialized";
-    case ERR_CANTWRITE:
-      return "Cannt write to file";
-    case ERR_NOMEM:
-      return "Out of memory";
-    case ERR_OUTOFRANGE:
-      return "Attempted to read past the bytestream size";
-    case ERR_RUNTIME:
-      return "Runtime error";
-    case ERR_UNKWFILE:
-      return "Unknown type of file";
-    default:
-      return "Unknown error";
-    }
-  }
-
-  static RetCode WriteToFile(const char *FileName, EvalSerializable& Object)
-  {
-    FILE *Fh;
-    RetCode Ret;
-    ByteStream Stream;
-
-    Ret = Object.Serialize(Stream);
-
-    if (Ret != SUCCESS) {
-      Object.m_LastInternalError = Ret;
-      return ERR_CANTSERIALIZE;
-    }
-
-    Fh = fopen(FileName, "wb");
-    if (Fh == nullptr)
-      return ERR_CANTOPEN;
-
-    if (fwrite(&Stream[0], Stream.size(), 1, Fh) != 1)
-      return ERR_CANTWRITE;
-
-    fflush(Fh);
-    fclose(Fh);
-
-    return SUCCESS;
-  }
+  static RetCode ReadFromFile(const char *FileName, EvalSerializable& Object);
+  static std::string RetCodeToString(RetCode ret);
+  static RetCode WriteToFile(const char *FileName, EvalSerializable& Object);
 
 protected:
-  explicit EvalSerializable() : m_LastInternalError(EvalSerializable::SUCCESS) {}
-  ~EvalSerializable() {}
+  explicit EvalSerializable();
+  ~EvalSerializable();
 
-  RetCode CheckHeader(const char*& Ptr, const char * const End) const
-  {
-    std::string Header;
-    std::string TN;
-
-    ReadData(Header, Ptr, End);
-    if (Header.compare(HEADER_TAG) != 0)
-      return ERR_UNKWFILE;
-
-    ReadData(TN, Ptr, End);
-    if (TN.compare(TypeName()) != 0)
-      return ERR_BADTYPE;
-
-    return SUCCESS;
-  }
+  RetCode CheckHeader(const char*& Ptr, const char * const End) const;
 
   virtual std::string TypeName() const = 0;
 
-  RetCode WriteHeader(ByteStream& Stream) const
-  {
-    RetCode Ret;
-
-    Ret = AddData(HEADER_TAG, Stream);
-    if (Ret != SUCCESS)
-      return Ret;
-
-    std::string TN = TypeName();
-    return AddData(TN, Stream);
-  }
+  RetCode WriteHeader(ByteStream& Stream) const;
 
   template<typename T>
   static RetCode AddData(const T& Data, ByteStream& Stream)
@@ -402,66 +266,8 @@ private:
     return _ReadData_fundamental(Data, Ptr, End, bool_<std::is_fundamental<T>::value>());
   }
 
-  static RetCode _ReadData(std::string& Data, const char*& Ptr, const char * const End, Identity<std::string>)
-  {
-    uint32_t Length;
-    char *Ch;
-
-    Length = *reinterpret_cast<const uint32_t*>(Ptr);
-    Ptr += sizeof(Length);
-
-    if (Ptr + Length > End)
-      return ERR_OUTOFRANGE;
-
-    if (Length == 0) {
-      Data = std::string();
-      return SUCCESS;
-    }
-
-    Ch = new char[Length+1];
-    if (Ch == nullptr)
-      return ERR_NOMEM;
-
-    memcpy(Ch, Ptr, Length);
-    Ch[Length] = '\0';
-    Data = std::string(Ch);
-    Ptr += Length;
-    delete[] Ch;
-
-    return SUCCESS;
-  }
-
-  static RetCode _ReadData(std::wstring& Data, const char*& Ptr, const char * const End, Identity<std::wstring>)
-  {
-    uint32_t Length;
-    uint32_t WLength;
-    wchar_t *Ch;
-
-    Length = *reinterpret_cast<const uint32_t*>(Ptr);
-    Ptr += sizeof(Length);
-
-    if (Ptr + Length > End)
-      return ERR_OUTOFRANGE;
-
-    if (Length == 0) {
-      Data = std::wstring();
-      return SUCCESS;
-    }
-    WLength = Length / sizeof(wchar_t);
-
-
-    Ch = new wchar_t[WLength + 1];
-    if (Ch == nullptr)
-      return ERR_NOMEM;
-
-    memcpy(Ch, Ptr, Length);
-    Ch[WLength] = '\0';
-    Data = std::wstring(Ch);
-    Ptr += Length;
-    delete[] Ch;
-
-    return SUCCESS;
-  }
+  static RetCode _ReadData(std::string& Data, const char*& Ptr, const char * const End, Identity<std::string>);
+  static RetCode _ReadData(std::wstring& Data, const char*& Ptr, const char * const End, Identity<std::wstring>);
 
   template<typename T>
   static RetCode _ReadData(std::vector<T>& Data, const char*& Ptr, const char * const End, Identity<std::vector<T>>)
