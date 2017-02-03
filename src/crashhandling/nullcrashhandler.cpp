2ac6386c1014a6adb8aacbfde0c4a927b3e21fb3
Michal Malý <malymi@natur.cuni.cz>
2017-02-03 15:59:46 +0100

    Wire up NullCrashHandler

--- src/crashhandling/nullcrashhandler.cpp
+++ src/crashhandling/nullcrashhandler.cpp
@@ -1,6 +1,22 @@
 #include "nullcrashhandler.h"
 
+NullCrashHandler::NullCrashHandler(const std::string &) :
+  CrashHandlerBase(""),
+  m_nullCrashInfo("")
+{
+}
+
+const std::string & NullCrashHandler::crashInfo() const
+{
+  return m_nullCrashInfo;
+}
+
 bool NullCrashHandler::install()
+{
+  return true;
+}
+
+bool NullCrashHandler::mainThreadCrashed() const
 {
   return true;
 }
--- src/crashhandling/nullcrashhandler.h
+++ src/crashhandling/nullcrashhandler.h
@@ -5,8 +5,15 @@
 
 class NullCrashHandler : public CrashHandlerBase {
 public:
+  explicit NullCrashHandler(const std::string &);
+
+  virtual const std::string & crashInfo() const override;
+  virtual bool mainThreadCrashed() const override;
   virtual bool install() override;
   virtual void uninstall() override;
+
+private:
+  const std::string m_nullCrashInfo;
 
 };
 
